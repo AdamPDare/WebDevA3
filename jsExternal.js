9a712f174dfb9a0c2f014a3180e44dc777bbf4c4
AdamPDare <71335091+AdamPDare@users.noreply.github.com>
2021-04-08 10:53:03 +0100

    first commit

--- jsExternal.js
+++ jsExternal.js
@@ -32,3 +32,28 @@ function setCookie(cname,cvalue,exdays) {
        }
     }
   }
+
+  function checkBrowser(){
+    let userAgentString= navigator.userAgent;
+    let chromeAgent = userAgentString.indexOf("Chrome")>-1;
+    let IExplorerAgent=userAgentString.indexOf("MSIE")>-1||
+    userAgentString.indexOf("rv:")>-1;
+    let firefoxagent=userAgentString.indexOf("Firefox")>-1;
+    let safariAgent=userAgentString.indexOf("Safari")>-1;
+
+    if((chromeAgent)&&(safariAgent)){
+        safariAgent=false;
+    }
+
+    let operaAgent=userAgentString.indexOf("OP")>-1;
+
+    if((chromeAgent)&&(operaAgent)){
+        chromeAgent=false;
+    }
+
+    document.querySelector(".output-safari").textContent=safariAgent;
+    document.querySelector(".output-chrome").textContent=chromeAgent;
+    document.querySelector(".output-ie").textContent=IExplorerAgent;
+    document.querySelector(".output-opera").textContent=operaAgent;
+    document.querySelector(".output-firefox").textContent=firefoxagent;
+}
